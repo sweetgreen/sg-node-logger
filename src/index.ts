81223e2a0efa5846e9e8dd0c9bffcfc6b4692095
Wesley Chiang <wes.chiang@sweetgreen.com>
2020-11-05 14:08:20 -0800

    Update release workflow 16

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// FIXME: publish to github repo not NPM and publish via GH actions and not CCI
 import winston from 'winston';
 
 const logger = winston.createLogger({
@@ -10,12 +9,11 @@ const logger = winston.createLogger({
     // - Write all logs with level `error` and below to `error.log`
     // - Write all logs with level `info` and below to `combined.log`
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
-//
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
