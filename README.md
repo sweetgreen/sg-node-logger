5d03e97f6be3e54f0b4a2ebd60ca68d114023b53
Wesley Chiang <wes.chiang@sweetgreen.com>
2020-11-05 12:12:21 -0800

    Initial working logger

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-PLACEHOLDER
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+
+- npm
+
+```sh
+npm install npm@latest -g
+```
+
+### Installation
+
+1. Add to your node project
+
+```sh
+yarn add @sweetgreen/sg-node-logger
+```
+
+<!-- USAGE EXAMPLES -->
+
+## Usage
+
+```javascript
+// CommonJS
+const { sgLogger } = require('@sweetgreen/sg-node-logger');
+
+// ES6
+import { sgLogger } from '@sweetgreen/sg-node-logger';
+
+sgLogger.info('salads');
+```
+
+_This logger is merely a wrapper around Winston, please refer to their [Documentation](https://github.com/winstonjs/winston)_
+
+<!-- ROADMAP -->
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-// FIXME: publish to github repo not NPM and publish via GH actions and not CCI
 import winston from 'winston';
 
-const logger = winston.createLogger({
+export const sgLogger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
@@ -10,21 +9,18 @@ const logger = winston.createLogger({
     // - Write all logs with level `error` and below to `error.log`
     // - Write all logs with level `info` and below to `combined.log`
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
-//
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(
+  sgLogger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
     })
   );
 }
-
-export default logger;
