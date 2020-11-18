f2e42cca58165747b3a21d3c996efce6c0a6e066
Wesley Chiang <wes.chiang@sweetgreen.com>
2020-11-17 16:15:04 -0800

    feat(index.ts): update logger to create log files; add more notes and fixmes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
+// TODO's
+// 1. update transports such that logs are saved to files
+// 2. wrap this with fast-redact (https://github.com/davidmarkclements/fast-redact) such that clients can pass keys that should be redacted
+// 3. why do we use morgan? I see that morgan can be integrated with winston.
+// 4. add tests
+
+// wants:
+// 1. log to console
+// 2. send logs to cloudwatch
+// 3. single config file that
+
 import winston from 'winston';
 
 export const sgLogger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: 'dont-forget-to-update-default-meta' },
   transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    // new winston.transports.File({ filename: 'combined.log' }),
+    // FIXME: confirm this is what we want with the wider engineering team; is this the right name? Is this the right location to output the log files?
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
+// NOTE: we can configure an already created logger by doing sgLogger.configure({...})
+
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== 'production') {
   sgLogger.add(
     new winston.transports.Console({
